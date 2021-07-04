788d3ed04158c7e7f329ec8e25e9c1c40e85983b
ERFANIUM <officialerfanshield@gmail.com>
2021-07-04 20:38:06 +0430

    run deno fmt

--- deps.ts
+++ deps.ts
@@ -1,7 +1,4 @@
-export {
-  createKey,
-  createPlugin,
-} from "denzo/mod.ts";
+export { createKey, createPlugin } from "denzo/mod.ts";
 export type { DenzoRequest } from "denzo/lib/request.ts";
 export type { DenzoReply } from "denzo/lib/reply.ts";
 export * as Vary from "https://deno.land/x/vary@1.0.0/mod.ts";
